d07c7b2cf48fcc908f0978a9059f42b36649ea10
Claudio Perroni <c.perroni@ifortech.com>
2022-12-13 14:12:40 +0100

    homepage

---
+++ views/components/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Tailwind -->
+    <link href="https://dev.labormedical.it/labortailwind/views/assets/output.css" rel="stylesheet">
+
+    <!-- Fontawesome -->
+    <link href="https://dev.labormedical.it/labortailwind/views/assets/vendor/font-awesome/css/all.min.css" rel="stylesheet">
+    <script src="https://dev.labormedical.it/labortailwind/views/assets/vendor/font-awesome/js/all.min.js"></script>
+    
+
+    <title>Labor Medical</title>
+</head>
+
+<body class="bg-gradient-to-r from-sky-100">
+
---
+++ views/components/navbar.php
@@ -0,0 +1,19 @@
+<nav class="flex flex-row justify-between items-center px-4">
+
+    <img src="https://labormedical.it/img/logo.png?v=1.1" alt="logo" class="h-[63px] w-[187px] ">
+
+    <ul class="hidden lg:flex flex-row gap-6 items-center">
+        <li class="text-base font-semibold cursor-pointer hover:text-blue-600">Corsi</li>
+        <li class="text-base font-semibold cursor-pointer hover:text-blue-600">In onda</li>
+        <li class="text-base font-semibold cursor-pointer hover:text-blue-600">Chi siamo</li>
+        <li class="text-base font-semibold cursor-pointer hover:text-blue-600">Notizie</li>
+        <li class="">
+            <button class="bg-blue-600 hover:bg-blue-500 text-slate-50 p-2 rounded">Accedi</button>
+        </li>
+    </ul>
+
+    <a href="#" class="lg:hidden flex flex-col items-center justify-center ">
+        <i class="fas fa-bars"></i>
+    </a>
+
+</nav>
